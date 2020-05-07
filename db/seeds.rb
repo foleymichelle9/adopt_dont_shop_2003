1012cb4d12cd09c8beb72d866a347b861baa00ba
michelle <foleymichelle9@gmail.com>
2020-05-07 14:16:36 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+shelter_1 = Shelter.create(name: "Perfect Paws Animal Shelter",
+                           address: "1470 N Main St",
+                           city: "Madison",
+                           state: "WI",
+                           zip: 53703)
+shelter_2 = Shelter.create(name: "Aristocats Cat Rescue",
+                           address: "230 W Washington Ave",
+                           city: "Madison",
+                           state: "WI",
+                           zip: 53703)
